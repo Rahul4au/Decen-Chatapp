db971293ba809c32a1506e85d0ccecea23326763
Rahul Singh <rahulsingh4au@gmail.com>
2025-01-24 00:27:46 +0530

    developed APIFeature

---
+++ chatapp_frontend/Context/constants.js
@@ -0,0 +1,7 @@
+//0x5DFd5eBcc3b467Ded20D14eeCB6813B520CE4389
+
+import ChatAppJSON from './ChatApp.json';
+
+export const ChatAppAddress = 0x5DFd5eBcc3b467Ded20D14eeCB6813B520CE4389;
+
+export const ChatAppABI = ChatAppJSON.abi ;
---
+++ chatapp_frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import Navbar from '../Components/Navbar/Navbar';
+// import Filter from '../Components/Filter/Filter';
+// import Error from '../Components/Error/Error';
+// import Friend from '../Components/Friend/Friend';
+// import UserCard from '../Components/UserCard/UserCard';
+// import Loader from '../Components/Loader/Loader';
+// import Model from '../Components/Model/Model';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App ;
